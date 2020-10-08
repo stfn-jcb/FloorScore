673e03408797723f2b7b4cc50e9813c07a3180b8
Marc White <marc.white@anu.edu.au>
2020-10-08 16:28:35 +1100

    Functioning period box highlighting

--- js/display-setup.js
+++ js/display-setup.js
@@ -70,7 +70,7 @@ function formatScreen () {
     timerFS = parseInt(matchTimer.css('font-size'));
     matchPeriod.css('top', tourneyLogoH+timerH+'px');
     matchPeriod.css('height', 0.65 * timerH + "px");
-    matchPeriod.css('line-height', 0.65 * timerH + "px");
+    // matchPeriod.css('line-height', 0.40 * timerH + "px");
     matchPeriod.css('font-size', 0.45 * timerH + "px");
     periodH = parseInt(matchPeriod.css('height'));
     periodBoxes = $( '.period-box' );
